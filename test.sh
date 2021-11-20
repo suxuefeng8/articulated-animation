a7b0395b09d4bda687aad4969e570ee843a1bc92
suxuefeng <suxuefeng@sogou-inc.com>
2021-11-20 14:55:51 +0800

    commit

--- test.sh
+++ test.sh
@@ -1,6 +1,16 @@
-driving="/search/odin/sxf/data/action_yani/test/yaniwhite_vpa_yani8_ac1_white_vi02_5.mp4"
-source="/search/odin/sxf/data/action_yani/frame/0001.jpg"
-model="latest.pth"
+#driving="/search/odin/sxf/data/action_yani/test/yaniwhite_vpa_yani8_ac1_white_vi02_5.mp4"
+#source="/search/odin/sxf/data/action_yani/frame/0001.jpg"
+#config="config/action-yani.yaml"
 
-python demo.py  --config config/action-yani.yaml --driving_video $driving --source_image $source --checkpoint $model
+driving="/search/odin/sxf/data/action_male/limu_video_seg/sogou_btp_m_ttvs_limu_0011.mp4"
+source="/search/odin/sxf/data/action_male/limu_video_seg/test/sogou_btp_m_ttvs_limu_0011/frame-000001.png"
+
+model="latest.pth"
+config="config/action-limu.yaml"
+
+rm -f tmp.mp4
+ffmpeg -i $driving -vf scale=-1:512,crop=512:512:200:0 tmp.mp4
+
+#python demo.py  --config $config --driving_video tmp.mp4 --source_image $source --checkpoint $model --img_shape 512,512 --mode standard
+python demo.py  --config $config --driving_video tmp.mp4 --source_image $source --checkpoint $model --img_shape 512,512 --mode avd
 
--- train.sh
+++ train.sh
@@ -4,4 +4,8 @@ set -eux
 
 #CUDA_VISIBLE_DEVICES=0,1,2,3 python run.py --config config/action-yani.yaml --device_ids 0,1,2,3 --checkpoint log/action-yani\ 30_10_21_07.06.40/00000399-cpk-reconstruction.pth  >action.log 2>&1 &
 #CUDA_VISIBLE_DEVICES=2,3 python -u run.py --config config/action-limu.yaml --device_ids 0,1 --mode train >limu.log 2>&1 &
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -u run.py --config config/action-limu.yaml --device_ids 0,1,2,3 --mode train >limu.log 2>&1 &
+
+#CUDA_VISIBLE_DEVICES=0,1,2,3 python -u run.py --config config/action-limu.yaml --device_ids 0,1,2,3 --mode train >limu.log 2>&1 &
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python run.py --checkpoint log/action-limu\ 10_11_21_09.46.01/00000249-cpk-reconstruction.pth --config config/action-limu.yaml --device_ids 0,1,2,3 --mode train_avd >limu_avd.log 2>&1 &
+
